6c7288261da3b01abb59e9c94a26f391a9aac37c
bruce11521 <bruce11521@gmail.com>
2019-10-14 16:14:59 +0800

    Initial Commit

---
+++ TRAtimeTable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TRAtimeTable
+//
+//  Created by 林伯翰 on 2019/10/14.
+//  Copyright © 2019 林伯翰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
